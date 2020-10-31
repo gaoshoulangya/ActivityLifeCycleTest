2dccbcf5a37dcaf2e4106282597727075ceb4a96
gaoshoulangya <1667575229@qq.com>
2020-10-31 16:10:37 +0800

    <feat> update android app

--- app/src/main/java/swust/edu/cn/activitylifecycletest/MainActivity.java
+++ app/src/main/java/swust/edu/cn/activitylifecycletest/MainActivity.java
@@ -29,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,DialogActivity.class);
                 startActivity(intent);
+
+
             }
         });
     }
